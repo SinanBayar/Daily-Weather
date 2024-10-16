283a5c032ab1f5f52f7f34eb8987927bb0aa21b7
Sinan Bayar <sinan@mudi.app>
2024-10-16 23:29:49 +0300

    denem

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
       <div>
-        <h1>About Us deneme deneme deneme deneme denegyfhfffeme deneme denem</h1>
+        <h1>About Us denemsssse deneme deneme deneme denegyfhfffeme deneme denem</h1>
       </div>
     );
   }
