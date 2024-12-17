d3bfec716040f633a23fc216ebaa0f66f3280fb2
Sinan Bayar <sinanbayar88@gmail.com>
2024-12-17 23:57:05 +0300

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
       <div>
-        <h1>About33343qwxadwadwswsssswqd443srrsdfdsfwsdasdfsfwdadawdss343434432</h1>
+        <h1>fix</h1>
       </div>
     );
   }
